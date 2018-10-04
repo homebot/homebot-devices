e72aa86d9b2433e91962d29c1de81611e7678a61
ladanov <ladanov_n@mail.ru>
2018-10-04 21:33:19 +0300

    fixed

--- src/controllers/devices.js
+++ src/controllers/devices.js
@@ -8,7 +8,7 @@ require('../models/device');
 
 const Device = mongoose.model('Device');
 
-exports.list = async function(req, res) {
+exports.list = async function (req, res) {
   //if (!req.currentUser.canRead(req.locals.user)) return response.sendForbidden(res);
 
   const query = Object.assign(request.getFilteringOptions(req, ['name']));
@@ -16,12 +16,12 @@ exports.list = async function(req, res) {
     let result = await Device.paginate(query, request.getRequestOptions(req));
     pagination.setPaginationHeaders(res, result);
     res.json(result.docs);
-  } catch(err) {
+  } catch (err) {
     return response.sendNotFound(res);
   };
 };
 
-exports.create = async function(req, res) {
+exports.create = async function (req, res) {
   // const user = req.locals.user;
   //if (!req.currentUser.canEdit(user)) return response.sendForbidden(res);
   try {
@@ -31,43 +31,47 @@ exports.create = async function(req, res) {
     // await user.save();
 
     return response.sendCreated(res, device);
-  } catch(err) {
+  } catch (err) {
     return response.sendBadRequest(res, err);
   }
 };
 
-exports.read = async function(req, res) {
+exports.read = async function (req, res) {
   try {
     let device = await Device.findById(req.params.id);
     //if (!req.currentUser.canRead(device)) return response.sendForbidden(res);
     res.json(device);
-  } catch(err){
+  } catch (err) {
     return response.sendNotFound(res);
   };
 };
 
-exports.update = async function(req, res) {
+exports.update = async function (req, res) {
   try {
     let device = await Device.findById(req.params.id);
     //if (!req.currentUser.canEdit(device)) return response.sendForbidden(res);
-    device = await Device.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    device = await Device.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    })
     mqtt.publish('/alisa', JSON.stringify({
       deviceId: device._id,
       turn: device.payload.turn || "off"
     }));
     res.json(device)
-  } catch(err){
-      response.sendBadRequest(res, err);
-    };
+  } catch (err) {
+    response.sendBadRequest(res, err);
+  };
 };
 
-exports.delete = async function(req, res) {
+exports.delete = async function (req, res) {
   try {
     let device = await Device.findById(req.params.id);
     //if (!req.currentUser.canEdit(device)) return response.sendForbidden(res);
     await Device.findByIdAndRemove(req.params.id);
-    res.json({ message: 'Device successfully deleted' });
-  } catch(err){
+    res.json({
+      message: 'Device successfully deleted'
+    });
+  } catch (err) {
     return response.sendNotFound(res);
   }
 };
